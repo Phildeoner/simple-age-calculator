7330655337cd11bd42dd3b363fcd4834942427ad
Yakubu Bobai Ephraim <phildeoner@gmail.com>
2023-07-29 10:53:50 +0100

    Create addition to be reurned in the function

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let a = parseInt(prompt("Enter first number: "));
 let b = parseInt(prompt("Enter second number: "));
 
 //create an addition function
-function addition() {}
-//The addition function will return the addition for input a and b.
+function addition() {
+  //The addition function will return the addition for input a and b.
+  return a + b;
+}
+
 // Invoke addition function to run the calculator
