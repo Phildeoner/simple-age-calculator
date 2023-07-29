97d89d65fed6d9ce93c53f148ffaa1d778a6368a
Yakubu Bobai Ephraim <phildeoner@gmail.com>
2023-07-29 10:49:45 +0100

    Create pseudocode for the calculator

---
+++ script.js
@@ -0,0 +1,4 @@
+// Create variables to accept first and second user input
+//create an addition function
+//The addition function will return the addition for input a and b.
+// Invoke addition function to run the calculator
