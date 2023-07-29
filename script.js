736eaa8ce80938b82de93de953144b6613f124bb
Yakubu Bobai Ephraim <phildeoner@gmail.com>
2023-07-29 10:51:36 +0100

    Create prompt for first and second user input

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // Create variables to accept first and second user input
+let a = parseInt(prompt("Enter first number: "));
+let b = parseInt(prompt("Enter second number: "));
 //create an addition function
 //The addition function will return the addition for input a and b.
 // Invoke addition function to run the calculator
